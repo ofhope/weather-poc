1cedea959c8902eb04536a6ecae10ec5afa8d8c2
Alexis Hope <alexis@cvofhope.com>
2023-12-07 16:44:44 +1100

    fix: hourle forecast style

--- src/components/forecast/styles.ts
+++ src/components/forecast/styles.ts
@@ -3,10 +3,12 @@ import styled from "styled-components";
 export const HourRow = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: center;
   gap: 12px;
   background: rgb(236,236,236);
   background: linear-gradient(120deg, rgba(236,236,236,1) 0%, rgba(185,185,185,1) 100%);
   border: 1px solid #FFF;
   border-radius: 10px;
+  color: rgba(44,45,53,1);
   padding: 0 24px 0 6px;
 `
