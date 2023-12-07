5c87c889358082481d9e30c0234ffd7e2f0303f6
Alexis Hope <alexis@cvofhope.com>
2023-12-07 15:32:58 +1100

    refactor: pull out logic to result component

---
+++ src/components/result-summary/ResultSummary.stories.ts
@@ -0,0 +1,62 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ResultSummary } from '.';
+import { BAD_INPUT_ERROR, GENERIC_ERROR } from '../../domain/types';
+
+const meta = {
+  title: 'layout/ResultSummary',
+  component: ResultSummary,
+  parameters: {
+    layout: 'fullscreen',
+  },
+  tags: ['autodocs'],
+
+} satisfies Meta<typeof ResultSummary>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Inital: Story = {
+  args: {
+    loading: false,
+    result: undefined
+  }
+};
+
+export const Loading: Story = {
+  args: {
+    loading: true,
+    result: undefined
+  }
+};
+
+export const Default: Story = {
+  args: {
+    loading: false,
+    result: {
+      description: "Foo Description",
+      resolvedAddress: "Foo Address",
+      currentConditions: {
+        datetime: '09:00:00',
+        temp: 17.5,
+        feelslike: 17,
+        humidity: 20,
+        icon: "cloudy"
+      },
+      hourlyForecast: []
+    }
+  }
+};
+
+export const BadInputError: Story = {
+  args: {
+    loading: true,
+    result: BAD_INPUT_ERROR
+  }
+};
+
+export const GenericError: Story = {
+  args: {
+    loading: true,
+    result: GENERIC_ERROR
+  }
+};
---
+++ src/components/result-summary/ResultSummary.tsx
@@ -0,0 +1,36 @@
+import { BAD_INPUT_ERROR, GENERIC_ERROR, WeatherResult } from "../../domain/types"
+import { Container } from "../container"
+import { Forecast } from "../forecast"
+import { Message } from "../message"
+import { Weather } from "../weather"
+
+interface WeatherProps {
+  loading: boolean,
+  result: WeatherResult
+}
+
+export const ResultSummary = ({ loading, result }: WeatherProps) => {
+
+  if (result === undefined) {
+    return <Message>Welcome to weather search. Type a city in the search input above to find current weather conditions.</Message>
+  }
+
+  if (loading) {
+    return (<Container>Loading...</Container>)
+  }
+
+  if (result === BAD_INPUT_ERROR) {
+    return <Message>There was an error searching for that city. Try again.</Message>
+  }
+  
+  if (result === GENERIC_ERROR) {
+    return <Message>Something went wrong. Try again later or contact the adminstrator.</Message>
+  }
+
+  return (
+    <Container>
+      <Weather description={result.description} location={result.resolvedAddress} icon={result.currentConditions.icon} temp={result.currentConditions.temp} />
+      <Forecast hours={result.hourlyForecast} />
+    </Container>
+  )
+}
