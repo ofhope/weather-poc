3300055847708c1b1892f5aadcbde24b71519552
Alexis Hope <alexis@cvofhope.com>
2023-12-07 15:27:12 +1100

    refactor: weather types to domain

--- src/domain/types.ts
+++ src/domain/types.ts
@@ -2,7 +2,7 @@
 export const NETWORK_ERROR = "NETWORK_ERROR"
 export const BAD_INPUT_ERROR = "BAD_INPUT_ERROR"
 
-type WeatherIcon = "snow" | "rain" | "fog" | "wind" | "cloudy" | "partly-cloudy-day" | "partly-cloudy-night" | "clear-day" | "clear-night"
+export type WeatherIcon = "snow" | "rain" | "fog" | "wind" | "cloudy" | "partly-cloudy-day" | "partly-cloudy-night" | "clear-day" | "clear-night"
 
 export interface WeatherApiHour {
   datetime: string
@@ -31,7 +31,7 @@ export interface CurrentWeather {
   hourlyForecast: WeatherApiHour[]
 }
 
-type WeatherResult = CurrentWeather | typeof NETWORK_ERROR | typeof BAD_INPUT_ERROR | undefined
+export type WeatherResult = CurrentWeather | typeof NETWORK_ERROR | typeof BAD_INPUT_ERROR | undefined
 
 export interface WeatherState {
   loading: boolean,
